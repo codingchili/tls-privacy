9d447a2028e2b44dc83bdbf70ff649e60da54aee
Robin Duda <rduda@kth.se>
2022-03-28 19:16:02 +0200

    enable importing of exported datasets.

--- analyzer/persistence.py
+++ analyzer/persistence.py
@@ -1,19 +1,24 @@
 import pathlib
 import logging
+import pandas
 
 from analyzer.format import daytime
 
 logger = logging.getLogger()
+base_path = './data'
 
 
 def data_export(loads, packets, name):
-    path = f"./data/{name}"
+    name = daytime() if name == 'daytime' else name
+    path = f"{base_path}/{name}"
     logger.info(f"persistence exported data to '{path}'")
     pathlib.Path(path).mkdir(parents=True, exist_ok=True)
 
-    packets.to_json(f"{path}/packets-{daytime()}.json", orient="index", indent=4)
-    loads.to_json(f"{path}/loads-{daytime()}.json", orient="index", indent=4)
+    packets.to_json(f"{path}/packets.json", orient="index", indent=4)
+    loads.to_json(f"{path}/loads.json", orient="index", indent=4)
 
 
-def data_import(loads, packets):
-    pass
+def data_import(name):
+    loads = pandas.read_json(f"{base_path}/{name}/loads.json", orient="index")
+    packets = pandas.read_json(f"{base_path}/{name}/packets.json", orient="index")
+    return loads, packets
--- analyzer/run.py
+++ analyzer/run.py
@@ -16,7 +16,8 @@ parser.add_argument('--interface', help='interface to listen on.')
 parser.add_argument('--ip', help='host to capture traffic from/to.')
 parser.add_argument('--ports', help='ports to capture traffic on.', nargs='?', const=1, default='80,443')
 parser.add_argument('--address', help='location of shared directory.', nargs='?', const=1, default='./bus/analyzer.bus')
-parser.add_argument('--dump', help='location of shared directory.')
+parser.add_argument('--dump', help='dump all data under the given data dir.', nargs='?', const='daytime', default=None)
+parser.add_argument('--load', help='loads the dump with the given name.')
 
 args = parser.parse_args()
 args.ports = args.ports.split(',')
@@ -33,7 +34,7 @@ async def main(args):
             capture = sniffer.filter
 
             if args.dump is not None:
-                data_export(packets, loads, args.dump)
+                data_export(loads, packets, args.dump)
 
             plot_all(loads, packets, capture)
         else:
@@ -47,6 +48,13 @@ async def main(args):
 
 
 try:
-    asyncio.run(main(args))
+    if args.load and args.interface:
+        logger.info('cannot specify both interface source and dataset to load.')
+    else:
+        if args.load:
+            loads, packets = data_import(args.load)
+            plot_all(loads, packets, args.load)
+        else:
+            asyncio.run(main(args))
 except KeyboardInterrupt:
     logger.info("shutting down.")
