431b76584c6f07dbdb30ff89f75fda957c4a106c
Robin Duda <robin.duda@idainfront.se>
2022-04-13 19:41:38 +0200

    args fix

--- analyzer/run.py
+++ analyzer/run.py
@@ -12,7 +12,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-async def main(args, monitor):
+async def run_sniffer_async(args, monitor):
     notifier = Notifier(9555)
     sniffer = Sniffer(args.interface, args.ip, args.ports.split(','))
 
@@ -25,9 +25,9 @@ async def main(args, monitor):
     await sniffer.stats()
 
 
-def run_main(args, monitor=False):
+def run_sniffer(args, monitor=False):
     try:
-        asyncio.run(main(args, monitor))
+        asyncio.run(run_sniffer_async(args, monitor))
     except KeyboardInterrupt:
         logger.info("shutting down.")
 
@@ -57,7 +57,7 @@ def sniff(args):
     if args.interface and args.interface not in Sniffer.interfaces():
         logger.info("given interface not available, list available with --eth.")
     elif args.interface:
-        run_main(args)
+        run_sniffer(args)
     else:
         logger.warning('no interface specified, exiting.')
 
@@ -65,7 +65,7 @@ def sniff(args):
 def monitor(args):
     if args.list:
         list_interfaces()
-    run_main(args, monitor=True)
+    run_sniffer(args, monitor=True)
 
 
 def learn(args):
@@ -87,9 +87,9 @@ list_parser = subparsers.add_parser('list', help="lists the available interfaces
 list_parser.set_defaults(func=list_interfaces)
 
 sniff_parser = subparsers.add_parser('sniff', help="capture network data to create data sets.")
-sniff_parser.add_argument('--ip', help='host to capture traffic from/to.', nargs='?', metavar='ADDR')
+sniff_parser.add_argument('interface', help='interface to listen on.', metavar='ETH')
+sniff_parser.add_argument('--ip', help='host to capture traffic from/to.', metavar='ADDR', const=1, default='127.0.0.1')
 sniff_parser.add_argument('--ports', help='ports to capture traffic on.', nargs='?', const=1, default='80,443')
-sniff_parser.add_argument('--interface', help='interface to listen on.', metavar='ETH')
 sniff_parser.add_argument('--dump', help='dump all data under the given ./data dir.', nargs='?', const='daytime',
                           default=None)
 sniff_parser.set_defaults(func=sniff)
@@ -105,10 +105,10 @@ learn_parser.add_argument('--feats', help='feature combination, discovers best i
 learn_parser.set_defaults(func=learn)
 
 monitor_parser = subparsers.add_parser('monitor', help="monitor traffic using the given model.")
+monitor_parser.add_argument('interface', help='interface to listen on.', metavar='ETH')
 monitor_parser.add_argument('set', help='the data set to use for training.', metavar='SET')
-monitor_parser.add_argument('--ip', help='host to capture traffic from/to.', metavar='ADDR')
+monitor_parser.add_argument('--ip', help='host to capture traffic from/to.', metavar='ADDR', const=1, default='127.0.0.1')
 monitor_parser.add_argument('--ports', help='ports to capture traffic on.', nargs='?', const=1, default='80,443')
-monitor_parser.add_argument('--interface', help='interface to listen on.', metavar='ETH')
 monitor_parser.set_defaults(func=monitor)
 
 args = parser.parse_args()
